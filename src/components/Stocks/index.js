90b799150343409668fd3da7f089ac04cb156e3a
austinmpask <austinmpask@gmail.com>
2024-02-29 13:20:46 -0800

    dne

--- src/components/Stocks/index.js
+++ src/components/Stocks/index.js
@@ -1,9 +1,26 @@
+import {
+  Redirect,
+  useHistory,
+} from "react-router-dom/cjs/react-router-dom.min";
+
 function Stocks() {
-  return (
-    <div className='comp orange'>
-      <h1>Stocks Component</h1>
-    </div>
-  );
+  const loggedIn = true;
+  const history = useHistory();
+  const handleClick = () => {
+    window.alert("Doing something");
+    history.push("/");
+  };
+
+  if (loggedIn) {
+    return (
+      <div className="comp orange">
+        <h1>Stocks Component</h1>
+        <button onClick={handleClick}>Home</button>
+      </div>
+    );
+  } else {
+    return <Redirect to="/not-logged-in"></Redirect>;
+  }
 }
 
 export default Stocks;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+const loggedIn = false;
 const Root = () => {
   return (
     <BrowserRouter>
@@ -16,5 +16,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
